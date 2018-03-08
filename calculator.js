cd22ddabd312ed7abe082e65d739713ca5c8bfbe
wallisidd@gmail.com <wallisidd@gmail.com>
2018-03-09 04:44:20 +0500

    Calculator

---
+++ calculator.js
@@ -0,0 +1,45 @@
+function btnadd(){
+    document.calculator.display.value+="+";
+    document.calculator.display.style.textalign = "right";
+
+}
+
+function btnsub(){
+    document.calculator.display.value +="-";
+    document.calculator.display.style.textalign = "right";
+    
+}
+
+function btnmult(){
+    document.calculator.display.value +="*";
+    document.calculator.display.style.textalign = "right";
+    
+}
+
+function btnmod(){
+    document.calculator.display.value +="%";
+    document.calculator.display.style.textalign = "right";
+    
+}
+
+function btnadd(){
+    document.calculator.display.value +="+";
+    document.calculator.display.style.textalign = "right";
+    
+}
+function btndot(){
+    document.calculator.display.value +=".";
+    document.calculator.display.style.textalign = "right";
+    
+}
+function btndiv(){
+    document.calculator.display.value +="/";
+    document.calculator.display.style.textalign = "right";
+    
+}
+function btnallclear(){
+    document.calculator.display.value ="";
+    document.calculator.display.style.textalign = "right";
+    
+}
+
